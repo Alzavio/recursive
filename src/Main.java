0461df174da03a35573fe831c46a7f9ea53a38ff
Alzavio <luiz.matthew187@gmail.com>
2023-12-03 13:09:27 -0800

    Factorial fixed

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,24 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    private static int count = 0;
-    public static void Test(int n) {
-        if (n < 31) {
-            Test(n + 1);
-            Test(n + 2);
-            Test(n + 3);
-        } else if (n == 31) {
-            count++;
+    /***
+     * 31 factorial
+     *
+     * @param n The counting parameter that's checked each time it increases
+     * @return Returns 1 if it reaches 31, itself, if below 31, and 0 if above
+     */
+    public static int AttemptedFactorial(int n) {
+        if (n == 31) {
+            return 1;
+        } else if (n < 31) {
+            return AttemptedFactorial(n+1) + AttemptedFactorial(n+2) + AttemptedFactorial(n+3);
+        } else {
+            return 0;
         }
     }
 
-
     public static void main(String[] args) {
-        Test(1);
-        System.out.println(count);
+        int test = AttemptedFactorial(1);
+        System.out.println(test);
     }
 }
