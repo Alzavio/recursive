a9e593b073d9c9ed3c399c0cb06adcdb901734bf
Alzavio <luiz.matthew187@gmail.com>
2023-12-09 17:41:06 -0800

    documentation

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ import java.util.HashMap;
 public class Main {
     static HashMap<Integer, Integer> cache = new HashMap<Integer, Integer>();
 
+    /***
+     * 31 factorial
+     *
+     * @param n The counting parameter that's checked each time it increases
+     * @return Returns 1 if it reaches 31, itself, if below 31, and 0 if above
+     */
     private static int AttemptedFactorial(int n) {
         if (cache.containsKey(n)) {
             return cache.get(n);
@@ -15,6 +21,7 @@ public class Main {
             result = AttemptedFactorial(n-1) + AttemptedFactorial(n-2)+1;
         }
 
+        // Store result in cache so it can be referenced if the same value is arrived at in the future
         cache.put(n, result);
         return result;
     }
